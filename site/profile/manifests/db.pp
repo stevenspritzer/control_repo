81819c745ce28d71b12a01d83aa4d897aa80db54
stevenspritzer <steven@stevenspritzer.com>
2020-04-13 01:33:09 -0400

    Update db.pp

--- site/profile/manifests/db.pp
+++ site/profile/manifests/db.pp
@@ -1,1 +1,8 @@
-class profile::db {}
+class profile::db {
+  class { '::mysql::server':
+  root_password           => 'strongpassword',
+  remove_default_accounts => true,
+  restart                 => true,
+  override_options        => $override_options
+}
+}
